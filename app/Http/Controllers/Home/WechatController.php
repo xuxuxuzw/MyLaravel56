8f1eb47d08a4bd6e3f5cca4045c38fa85b168370
xuzhaowen <yangzeloveyou@gmail.com>
2019-04-20 12:52:22 +0800

    no message

--- app/Http/Controllers/Home/WechatController.php
+++ app/Http/Controllers/Home/WechatController.php
@@ -27,12 +27,13 @@ class WechatController extends Controller
     public function oauth_callback()
     {
         //获取app实例
-        /** @var \EasyWeChat\OfficialAccount\Application $app*/
+        /** @var \EasyWeChat\OfficialAccount\Application $app */
         $app = app('wechat.official_account');
         $oauth = $app->oauth;
         // 获取 OAuth 授权结果用户信息
         $user = $oauth->user();
-        session(['openid' => $user->getId(),'nickname'=>$user->getNickname()]);
+        session(['openid' => $user->getId()]);
+        session('nickname', $user->getNickname());
         header('location:' . $this->host_url());
     }
 
