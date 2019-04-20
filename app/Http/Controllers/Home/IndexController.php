2e23b09f89ae76ea2744533beef21a48b9d14d8c
xuzhaowen <yangzeloveyou@gmail.com>
2019-04-20 12:50:22 +0800

    no message

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -1,28 +1,32 @@
 <?php
+
 namespace App\Http\Controllers\Home;
+
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use EasyWeChat\Factory;
+
 class IndexController extends Controller
 {
 
     /* 首页入口
      * @return page
      */
-    public function index(){
+    public function index()
+    {
         //获取配置信息
-        $app                = app('wechat.official_account');
-        $oauth              = $app->oauth;
-		
-		//如果没有openid则调起网页授权
-        if (empty(session('openid'))) {
-            session(['target_url'=>'/']);
+        $app = app('wechat.official_account');
+        $oauth = $app->oauth;
+
+        //如果没有openid则调起网页授权
+        if (empty(session('nickname')) || empty(session('openid'))) {
+            session(['target_url' => '/']);
             return $oauth->redirect();
-        }else{
-            return view('home.index.index',['openid' => session('openid'),'nickname'=>session('nickname')]);
+        } else {
+            return view('home.index.index', ['openid' => session('openid'), 'nickname' => session('nickname')]);
         }
     }
 
-    
+
 }
